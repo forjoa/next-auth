a059dc42c9910dea94e871845bfb665c65aa8165
Rebeca\Rebe y Joa <joaquin_trujillo@icloud.com>
2024-02-06 19:49:45 +0100

    auth working correctly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,57 @@
+import { redirect } from 'next/navigation'
+import { getSession, login, logout } from '../../lib'
 
-export default function Home() {
+export default async function Home() {
+  const session = await getSession()
   return (
-    <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Hello world!
+    <main className='flex min-h-screen flex-col p-24'>
+      <form
+        action={async (formData) => {
+          'use server'
+          await login(formData)
+          redirect('/')
+        }}
+      >
+        <input
+          type='text'
+          name='name'
+          id='name'
+          placeholder='Name'
+          className='border-2 border-solid border-black p-2 rounded-md'
+        />
+        <br />
+        <input
+          type='email'
+          name='email'
+          id='email'
+          placeholder='Email'
+          className='border-2 border-solid border-black p-2 rounded-md'
+        />
+        <br />
+        <button
+          type='submit'
+          className='border-2 border-solid bg-blue-500 text-white rounded-md px-4 py-2'
+        >
+          Login
+        </button>
+      </form>
+
+      <form
+        action={async () => {
+          'use server'
+          await logout()
+          redirect('/')
+        }}
+      >
+        <button
+          type='submit'
+          className='border-2 border-solid bg-red-500 text-white rounded-md px-4 py-2'
+        >
+          Logout
+        </button>
+      </form>
+
+      <pre>{JSON.stringify(session, null, 2)}</pre>
     </main>
   )
 }
